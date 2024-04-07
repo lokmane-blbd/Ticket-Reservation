dc47b47ba70c558fe77235c4ae36ac017579947d
Lokmane boulbadaoui <lokmaneblbd01@gmail.com>
2024-04-07 20:57:17 +0300

    Add files via upload

---
+++ Ticket-Reservation-System-main/Ticket-Reservation-System/Program-2.cs
@@ -0,0 +1,19 @@
+using Ticket_Reservation_System.Forms;
+
+namespace Ticket_Reservation_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new PlaneKoltukEconomy());
+        }
+    }
+}
